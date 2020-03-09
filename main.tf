5ab7e26cb4b938ab2efac82e053aea138ecc8093
Atanas Chuchulev <atanas.v4@gmail.com>
2020-03-09 14:26:15 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -28,6 +28,6 @@ resource "aws_instance" "server" {
   subnet_id     = each.key # note: each.key and each.value are the same for a set
 
   tags = {
-    Range = "Server in ${each.key}"
+    Range = "Server IP from ${each.key}"
   }
 }
